b5e5e857b945bfa99b9ca028ffee5fcf8a4ea108
ScorchStorm <ove18005@byui.edu>
2024-04-27 23:36:48 -0600

    I completed the assignment Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter;
+        string sign;
+        Console.Write("What is your grade percentage? ");
+        string percentageString = Console.ReadLine();
+        int percentage = int.Parse(percentageString);
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        if (percentage%10 <= 3)
+        {
+            sign = "-";
+        }
+        else if (percentage%10 >= 7)
+        {
+            sign = "+";
+        }
+        else
+        {
+            sign = "";
+        }
+        if (letter == "A" && sign == "+" || letter == "F")
+        {
+            sign = "";
+        }
+        Console.WriteLine($"Your grade is a {letter}{sign}.");
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you did not pass the class. You can pass the class if you take it again and try harder.");
+        }
     }
 }
