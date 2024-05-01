c253e45754a35f1b9c55efa2fb27a05ba013b05c
ScorchStorm <ove18005@byui.edu>
2024-05-01 01:44:14 -0600

    Prep4 Assignment has been finished with both stretch challenges completed

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int number = 1; //The value of "number" in this line won't actually impact anything, it is just here so that the loop will start.
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            numbers.Add(number);
+        }
+        Console.WriteLine($"The sum is: {numbers.Sum()}");
+        Console.WriteLine($"The average is: {numbers.Sum()/numbers.Count()}");
+        Console.WriteLine($"The largest number is: {numbers.Max()}");
     }
 }
