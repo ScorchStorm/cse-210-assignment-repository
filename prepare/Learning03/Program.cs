b8827a421707401d62e6f28eb37380c6c118b494
ScorchStorm <ove18005@byui.edu>
2024-05-20 15:54:34 -0600

    I finished and tested the Learning03 assignment.

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -2,8 +2,28 @@ using System;
 
 class Program
 {
+    private Fraction _fraction1;
+    private Fraction _fraction2;
+    private Fraction _fraction3;
+    private Fraction _fraction4;
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Program _program = new Program();
+        _program.Run();
+    }
+    private void Run()
+    {
+        _fraction1 = new Fraction();
+        Console.WriteLine(_fraction1.GetFractionString());
+        Console.WriteLine(_fraction1.GetDecimalValue());
+        _fraction2 = new Fraction(6);
+        Console.WriteLine(_fraction2.GetFractionString());
+        Console.WriteLine(_fraction2.GetDecimalValue());
+        _fraction3 = new Fraction(6, 7);
+        Console.WriteLine(_fraction3.GetFractionString());
+        Console.WriteLine(_fraction3.GetDecimalValue());
+        _fraction4 = new Fraction(1, 3);
+        Console.WriteLine(_fraction4.GetFractionString());
+        Console.WriteLine(_fraction4.GetDecimalValue());
     }
 }
